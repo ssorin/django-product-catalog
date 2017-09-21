8f53abb93f104b4cbb7e3e401847b14639934bf9
ssorin <ssorin@dwdlc.com>
2017-09-22 00:48:49 +0200

    update templates

--- product_catalog/settings.py
+++ product_catalog/settings.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 
 
-PAGINATION = getattr(settings, 'PRODUCT_CATALOG_PAGINATION', 10)
+PAGINATION = getattr(settings, 'PRODUCT_CATALOG_PAGINATION', 2)
 
 PRODUCT_BASE_MODEL = getattr(settings, 'PRODUCT_CATALOG_PRODUCT_BASE_MODEL',
                              'product_catalog.models.product_abstract.AbstractProduct')
--- product_catalog/views/category.py
+++ product_catalog/views/category.py
@@ -12,7 +12,6 @@ class CategoryListView(ListView):
     """ """
     model = Category
     context_object_name = "category_list"
-    paginate_by = PAGINATION
 
     def get_context_data(self, **kwargs):
         context = super(CategoryListView, self).get_context_data(**kwargs)
